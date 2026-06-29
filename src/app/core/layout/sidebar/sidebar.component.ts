201bfb8209e6ba2f73185fcac49e240296ff9907
Thiane7 <thianalingueregueye@gmail.com>
2026-06-29 18:10:11 +0100

    feat: complete angular frontend and spring boot integration with cors configuration

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // <-- On importe le moteur de routage
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule], // <-- On injecte RouterModule ici
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  // Ton composant est maintenant équipé pour la navigation !
 }
